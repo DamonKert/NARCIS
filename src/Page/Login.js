9d7d34cd2dea3feb996fc656c51157ddfbb82c1e
KimChhengChhim <114902030+KimChhengChhim@users.noreply.github.com>
2024-03-06 22:14:00 +0700

    dol kleng cookie

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -1,79 +1,73 @@
 import React, { useState } from "react";
-import { LoginOutlined } from "@ant-design/icons";
+import { CardHeader } from 'react-bootstrap'
 import { Steps, Form, Input, Button } from "antd";
-
+import { GetRequired } from '../Asset/Validated/Validated';
+import Cookies from 'js-cookie';
+import { List_Image } from '../Image/ListImage';
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 export default function Login() {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  const doLogin = (e) => {
-    e.preventDefault();
-    console.log(username);
-    console.log(password);
-    if (username != null && password != null) {
+  const [errorMessage, setErrorMessage] = useState('');
+  const doLogin = (data) => {
+    console.log(data);
+    console.log('fasdf');
+    if (data.Username != null && data.Password != null) {
       console.log('in');
-      var data ={
-        Username: username,
-        Password: password,
-      }
-      console.log(data);
-      
       fetch(`https://localhost:7266/api/Login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({Username: username, Password: password})
+        body: JSON.stringify(data)
       })
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
+          if (data.StatusCode === 401) {
+            setErrorMessage(data.Message);
+          } else if (data.StatusCode === 200) {
+            Cookies.set('userToken', data.Token);
+            console.log(Cookies.get('userToken'));
+          }
         })
         .catch((error) => {
           console.log(error);
         });
     }
   };
-  const handleUsernameInput = (e) => {
-    console.log(e.target.value);
-    setUsername(e.target.value);
-  };
-  const handlePasswordInput = (e) => {
-    console.log(e.target.value);
-    setPassword(e.target.value);
-  };
   return (
     <div className="container vh-100 d-flex justify-content-center">
-      <div className=" d-flex justify-content-center align-items-center">
-        <Form>
-          <Form.Item
-            label="Username"
+      <div className=" d-flex justify-content-center align-items-center d-flex flex-column">
+        <LazyLoadImage className='M-Logo-Responsive' style={{ width: '250px', height: 'auto' }} src={List_Image.Logo} alt={"Narcis Logo"} />
+       <br></br>
+        <Form onFinish={doLogin} className="d-flex flex-column">
+          <Form.Item 
+            label={<span className ="fs-4">Username</span>}
             name={'Username'}
-            rules={[
-              {
-                required: true,
-                type: "username",
-                message: "Please enter a valid username",
-              },
-            ]}
+            rules={[{
+              ...GetRequired("Username")
+
+            }]}
           >
-            <Input value={username} name="Username" onChange={handleUsernameInput} />
+            <Input name="Username" />
           </Form.Item>
           <Form.Item
-            label="Password"
+      
+            label={<span className ="fs-4">Password</span>}
             name={'Password'}
-            rules={[
-              {
-                required: true,
-                type: "password",
-                message: "Please enter your password",
-              },
-            ]}
-          >
-            <Input.Password value={password} name="Password" onChange={handlePasswordInput} />
+
+            rules={[{
+              ...GetRequired("Password")
+            }, {
+              min: 5,
+              message: "Password must be at least 8 letter"
+            }]}>
+            <Input.Password name="Password" />
           </Form.Item>
-          <Button type="primary" onClick={doLogin}>
+          {errorMessage ? (<span className="text-danger mb-3">{errorMessage}</span>) : ('')}
+          <Button type="primary"   htmlType='submit'>
             Login
           </Button>
+
         </Form>
       </div>
     </div>
